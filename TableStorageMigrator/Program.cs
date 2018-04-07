136fd2b761bd6515d64320462cd2b4e021ef5b8c
Andrey Migin <andrey.migin@lykkex.com>
2018-04-07 21:57:30 +0300

    Fixed naming

--- TableStorageMigrator/Program.cs
+++ TableStorageMigrator/Program.cs
@@ -9,7 +9,7 @@ namespace TableStorageMigrator
             var srcConnString = Environment.GetEnvironmentVariable("SrcConnString");
             if (string.IsNullOrEmpty(srcConnString))
                 throw new Exception("Please specift 'SrcConnString' env variable");
-            
+
             var destConnString = Environment.GetEnvironmentVariable("DestConnString");
             if (string.IsNullOrEmpty(destConnString))
                 throw new Exception("Please specift 'DestConnString' env variable");
@@ -20,25 +20,27 @@ namespace TableStorageMigrator
 
 
             var tables = tablesFromEnvVariable.Split('|');
-            
+
 
             foreach (var tableToCopy in tables)
             {
-                Console.WriteLine("Loading entities for table: "+tableToCopy);
+                Console.WriteLine("Copying table: " + tableToCopy);
 
 
                 var srcTable = srcConnString.GetAzureTable(tableToCopy);
                 var destTable = destConnString.GetAzureTable(tableToCopy);
-                
+
                 var copyPasteEngine = new CopyPasteEngine(srcTable, destTable);
 
 
                 copyPasteEngine.ReadTask.Wait();
                 copyPasteEngine.WriteTask.Wait();
 
+                Console.WriteLine("Done with table: " + tableToCopy);
+
 
             }
-            
+
             Console.WriteLine("Done....");
             Console.ReadLine();
         }
