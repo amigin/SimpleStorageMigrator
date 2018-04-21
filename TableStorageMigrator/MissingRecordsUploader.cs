0478964c9baa54710617379be59e98d280b110b3
Andrey Migin <andrey.migin@lykkex.com>
2018-04-22 01:07:51 +0300

    Added missing records uploader engine

---
+++ TableStorageMigrator/MissingRecordsUploader.cs
@@ -0,0 +1,84 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using MoreLinq;
+
+namespace TableStorageMigrator
+{
+
+    public static class MissingRecordsUploader
+    {
+
+        public static void Upload(string srcConnString, string destConnString)
+        {
+
+            foreach (var srcTable in SettingsReader.GetSrcTables(srcConnString))
+            {
+                Console.WriteLine("Loading table: " + srcTable.CloudTable);
+
+                var buffer = new Dictionary<string, Dictionary<string, DynamicTableEntity>>();
+
+                srcTable.GetEntitiesByChunkAsync(chunk =>
+                {
+
+                    foreach (var entity in chunk)
+                    {
+                        if (!buffer.ContainsKey(entity.PartitionKey))
+                            buffer.Add(entity.PartitionKey, new Dictionary<string, DynamicTableEntity>());
+                        buffer[entity.PartitionKey].Add(entity.RowKey, entity);
+
+                        Console.WriteLine("Loaded entities: " + chunk.Length);
+                    }
+
+                    return Task.FromResult(0);
+
+                }).Wait();
+
+                var destTable = destConnString.GetAzureTable(srcTable.TableName);
+
+                destTable.GetEntitiesByChunkAsync(chunk =>
+                {
+
+                    var removed = 0;
+
+                    foreach (var entity in chunk)
+                    {
+                        if (!buffer.ContainsKey(entity.PartitionKey))
+                            continue;
+
+                        var partition = buffer[entity.PartitionKey];
+
+                        if (partition.ContainsKey(entity.RowKey))
+                            partition.Remove(entity.RowKey);
+
+                        if (partition.Count == 0)
+                            buffer.Remove(entity.PartitionKey);
+
+                        removed--;
+
+                    }
+
+                    Console.WriteLine($"Loaded {chunk.Length} at dest table. Removed: " + removed);
+
+                    return Task.FromResult(0);
+
+                }).Wait();
+
+
+                foreach (var kvp in buffer)
+                {
+                    Console.WriteLine("Syncinc Partition: " + kvp.Key);
+
+                    foreach (var chunk in kvp.Value.Values.Batch(1000))
+                        destTable.InsertAsync(chunk.ToArray()).Wait();
+
+                }
+            }
+        }
+
+    }
+
+}
